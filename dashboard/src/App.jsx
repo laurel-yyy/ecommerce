7c42ff2b885d4dd79aea86bd33db056d3e7850ef
laurel-yyy <yanglujia2002@outlook.com>
2025-01-21 00:22:53 -0800

    initialize file

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-3xl border-l-4 border-blue-500 p-4">
+        <p>Hello world</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
